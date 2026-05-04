598a67c0d863a4d697e01521b25861f327856046
挡泥板34 <778414792@qq.com>
2026-05-05 00:12:08 +0800

    2026/5/5

--- rag_modules/__init__.py
+++ rag_modules/__init__.py
@@ -1,9 +1,11 @@
 from .email_access_and_synchronization_module import EmailAccessAndSynchronizationModule
 from .ai_analysis_core_module import AiAnalysisCoreModule
 from .milvus_connection_module import MilvusConnectionModule
+from .vector_storage_and_memory_system_module import VectorStorageAndMemorySystemModule
 
 __all__ = [
     EmailAccessAndSynchronizationModule,
     AiAnalysisCoreModule,
     MilvusConnectionModule,
+    VectorStorageAndMemorySystemModule,
 ]
